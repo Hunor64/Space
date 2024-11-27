6c47a342a3884ed16b47ea053a17c591b678ed57
hunor64 <nhunor06@gmail.com>
2024-11-27 13:27:14 +0100

    Add Navbar component and update routing in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,30 @@ import Home from "./pages/Home"
 import Crew from "./pages/Crew"
 import Destination from "./pages/Destination"
 import Technology from "./pages/Technology"
+import Navbar from "./components/Navbar"
+
+export const ROUTES = {
+  homePage: {path: '/', title: 'Home'},
+  crewPage: {path: '/crew', title: 'Crew'},
+  destinationPage: {path: '/destination', title: 'Destination'},
+  techPage: {path: '/tech', title: 'Technology'}
+}
+
 
 const App = () => {
-  return (
-    <>
-<Router>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/crew" element={<Crew />} />
-    <Route path="/destination" element={<Destination />} />
-    <Route path="/tech" element={<Technology />} />
-  </Routes>
-</Router>
-    </>
-  )
+	return (
+		<>
+			<Router>
+        <Navbar />
+				<Routes>
+					<Route path={ROUTES.homePage.path} element={<Home />} />
+					<Route path={ROUTES.crewPage.path} element={<Crew />} />
+					<Route path={ROUTES.destinationPage.path} element={<Destination />} />
+					<Route path={ROUTES.techPage.path} element={<Technology />} />
+				</Routes>
+			</Router>
+		</>
+	)
 }
 
 export default App
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,15 @@
+import { ROUTES } from "../App"
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        <Link to={ROUTES.homePage.path}>
+        <strong>00</strong>{ROUTES.homePage.title}</Link>
+        <Link to={ROUTES.crewPage.path}><strong>01</strong>{ROUTES.crewPage.title}</Link>
+        <Link to={ROUTES.destinationPage.path}><strong>02</strong>{ROUTES.destinationPage.title}</Link>
+        <Link to={ROUTES.techPage.path}><strong>03</strong>{ROUTES.techPage.title}</Link>
+    </nav>
   )
 }
 
