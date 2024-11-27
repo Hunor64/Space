ddcefec227a35c5c00ba29ea95a7ff8424a5e7b2
hunor64 <nhunor06@gmail.com>
2024-11-27 14:30:58 +0100

    Implement responsive Navbar with mobile and desktop views

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,49 @@
 import { ROUTES } from "../App"
 import { Link } from "react-router-dom"
 import styles from "./Navbar.module.css"
+import { useState } from "react"
 
 const Navbar = () => {
+	const [isMobile, setIsMobile] = useState(window.innerWidth < 1024)
+
+	window.addEventListener("resize", () => {
+		setIsMobile(window.innerWidth < 1024)
+	})
+
+	return <>{isMobile ? <MobileNavbar /> : <DesktopNavbar />}</>
+}
+
+const MobileNavbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
 	return (
-        <>
-        <DesktopNavbar />
-        </>
+		<nav className={styles.mobile}> 
+			<div className={styles.hamburger} onClick={()=>{setIsOpen(!isOpen)}}></div>
+        
+			<div className={styles.links} style={{display: isOpen ? "flex" : "none"}}>
+				<Link to={ROUTES.homePage.path}>
+					<strong>00</strong>
+					{ROUTES.homePage.title}
+				</Link>
+				<Link to={ROUTES.crewPage.path}>
+					<strong>01</strong>
+					{ROUTES.crewPage.title}
+				</Link>
+				<Link to={ROUTES.destinationPage.path}>
+					<strong>02</strong>
+					{ROUTES.destinationPage.title}
+				</Link>
+				<Link to={ROUTES.techPage.path}>
+					<strong>03</strong>
+					{ROUTES.techPage.title}
+				</Link>
+			</div>
+		</nav>
 	)
 }
 
 const DesktopNavbar = () => {
-    return (
+	return (
 		<nav className={styles.desktop}>
 			<div>
 				<Link to={ROUTES.homePage.path}>
