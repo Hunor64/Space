e4a4665fb26bcef2c741bfd50e80cf0c1416d6f9
hunor64 <nhunor06@gmail.com>
2024-11-27 13:07:47 +0100

    Add routing and new pages for Space application

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router"
+import Home from "./pages/Home"
+import Crew from "./pages/Crew"
+import Destination from "./pages/Destination"
+import Technology from "./pages/Technology"
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+<Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/crew" element={<Crew />} />
+    <Route path="/destination" element={<Destination />} />
+    <Route path="/tech" element={<Technology />} />
+  </Routes>
+</Router>
+    </>
   )
 }
 
