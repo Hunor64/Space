c7c1e4343f25e33c0e0f3f54f29df37be17c8068
hunor64 <nhunor06@gmail.com>
2024-11-27 13:51:28 +0100

    Refactor Navbar component to include DesktopNavbar and add styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,38 @@
 import { ROUTES } from "../App"
 import { Link } from "react-router-dom"
+import styles from "./Navbar.module.css"
 
 const Navbar = () => {
-  return (
-    <nav>
-        <Link to={ROUTES.homePage.path}>
-        <strong>00</strong>{ROUTES.homePage.title}</Link>
-        <Link to={ROUTES.crewPage.path}><strong>01</strong>{ROUTES.crewPage.title}</Link>
-        <Link to={ROUTES.destinationPage.path}><strong>02</strong>{ROUTES.destinationPage.title}</Link>
-        <Link to={ROUTES.techPage.path}><strong>03</strong>{ROUTES.techPage.title}</Link>
-    </nav>
-  )
+	return (
+        <>
+        <DesktopNavbar />
+        </>
+	)
+}
+
+const DesktopNavbar = () => {
+    return (
+		<nav className={styles.desktop}>
+			<div>
+				<Link to={ROUTES.homePage.path}>
+					<strong>00</strong>
+					{ROUTES.homePage.title}
+				</Link>
+				<Link to={ROUTES.crewPage.path}>
+					<strong>01</strong>
+					{ROUTES.crewPage.title}
+				</Link>
+				<Link to={ROUTES.destinationPage.path}>
+					<strong>02</strong>
+					{ROUTES.destinationPage.title}
+				</Link>
+				<Link to={ROUTES.techPage.path}>
+					<strong>03</strong>
+					{ROUTES.techPage.title}
+				</Link>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
